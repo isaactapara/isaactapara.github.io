68b46745e1797e90c263e232ada19906b2136482
isaactapara <isaactapara@gmail.com>
2025-10-13 13:39:49 +0300

    Portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/isaactapara.github.io/',
   server: {
     port: 3000,
     open: true
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    assetsDir: 'assets',
   }
 })
